07a07d92a7999af0b9cd5c9fb93297b089b6e30d
leo <lshao30@gmail.com>
2014-06-29 14:54:49 -0400

    change heading a little
    changed heading a bit

--- js/controllers/navigation.js
+++ js/controllers/navigation.js
@@ -1,40 +1,41 @@
 function NavigationController($scope,$rootScope, $location,$timeout,$http) {
-    
-     $scope.navClass = function (page) {
-        var currentRoute = $location.path().substring(1) || 'home';
-        return page === currentRoute ? 'active' : '';
-    };        
-   $scope.balance = "";
-   var balance= 0;
-     (function tick() {
-    $http({method:'GET', url:'https://blockchain.info/unspent?active=1GDHkj6EoeQiG3HeUgdUnxLgL8pj1A2yAp',headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).success(function(data, status, headers, config) {
 
-      for(var i in data.unspent_outputs){
-      
-         balance +=data.unspent_outputs[i].value;
-      }
-        //console.log(balance);
-      $scope.balance=balance/ 100000000;
-      balance=0;
+	$scope.navClass = function (page) {
+		var currentRoute = $location.path().substring(1) || 'home';
+		return page === currentRoute ? 'active' : '';
+	};        
+	$scope.balance = "";
 
-              $rootScope.balance =$scope.balance*100000000;
-          console.log($scope.balance);
-      //console.log(data.unspent_outputs[1].value);
-     // $scope.value=data.unspent_outputs[0].value;
-        $timeout(tick, 10000);
-    }).
+	var balance= 0;
+	(function tick() {
+		$http({method:'GET', url:'https://blockchain.info/unspent?active=1GDHkj6EoeQiG3HeUgdUnxLgL8pj1A2yAp',headers : { 'Content-Type': 'application/x-www-form-urlencoded' }}).success(function(data, status, headers, config) {
+
+			for(var i in data.unspent_outputs){
+
+			balance +=data.unspent_outputs[i].value;
+			}
+			//console.log(balance);
+			$scope.balance=balance/ 100000000;
+			balance=0;
+
+			$rootScope.balance =$scope.balance*100000000;
+			console.log($scope.balance);
+			//console.log(data.unspent_outputs[1].value);
+			// $scope.value=data.unspent_outputs[0].value;
+			$timeout(tick, 10000);
+		}).
 
 
-    error(function(data, status, headers, config) {
-      console.log(data);
-        console.log(status);
-          console.log(headers);
-       $scope.balance=0;
-               $timeout(tick, 20000);
-    });
-          })();
-		  
-	$scope.doInit=true;
-          //test
-    
+		error(function(data, status, headers, config) {
+		console.log(data);
+		console.log(status);
+		console.log(headers);
+		$scope.balance=0;
+		$timeout(tick, 20000);
+		});
+	})();
+  
+
+  //test
+
 }
