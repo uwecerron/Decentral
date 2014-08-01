3511828e6150b2eee25a0fd5e0803faba0366b3f
leo <lshao30@gmail.com>
2014-07-31 21:14:30 -0400

    home error handling
    cleaned up code a little

--- js/controllers/home.js
+++ js/controllers/home.js
@@ -1,5 +1,6 @@
 cApp.controller('Home', function($scope,$rootScope,modals,Blockchaininfo,Wallet,WalletManager,DecentralStorage) {
-  
+	
+	/*******************Home init*********************/
     $scope.pageClass = 'page-home';
     $scope.message = 'Choose Your Wallet';
     $scope.curWallet = WalletManager.getCurrentWallet();
@@ -10,13 +11,15 @@ cApp.controller('Home', function($scope,$rootScope,modals,Blockchaininfo,Wallet,
     })
     var backupFile;
 	$scope.currentAddress = "1Yj564jDqoB6L7hg5ETYKhqRsB65WrWPB";
-    var wallet1=new Wallet("uwe1");	
-
-    //var Block2= new Blockchaininfo();
-    //var addresses=['1Af7Xx9hpqS2GBLY6swqe2fsMmNgPxzAPk','1Yj564jDqoB6L7hg5ETYKhqRsB65WrWPB'];
-    //Block2.multiAddr(addresses);
-
-    /*export current wallet*/
+	$scope.isActive = true;
+	
+	setTimeout(function() {
+		var el = document.getElementById('first');
+		angular.element(el).triggerHandler('click');
+    },0);
+    /*******************Home init end*********************/
+	
+	/*export current wallet*/
     function download(filename, data) {
         var a = document.createElement('a');
         a.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(data));
@@ -25,11 +28,11 @@ cApp.controller('Home', function($scope,$rootScope,modals,Blockchaininfo,Wallet,
     }
 
     $scope.backup= function(){
-     var data = wallet1.getAddresses();
-     var fileName = wallet1.Name;
-     download(fileName+'.json', JSON.stringify(data)); 
+		var data = wallet1.getAddresses();
+		var fileName = wallet1.Name;
+		download(fileName+'.json', JSON.stringify(data)); 
     }
-    //import wallet
+
     $scope.import = function(){
         modals.open('modalpassword');
         var f = document.getElementById('file').files[0];
@@ -44,49 +47,43 @@ cApp.controller('Home', function($scope,$rootScope,modals,Blockchaininfo,Wallet,
         } 
         r.readAsText(f);
     }
-    setTimeout(function() {
-      var el = document.getElementById('first');
-       angular.element(el).triggerHandler('click');
-    },0);
-
-     var obj = {a: 123, b: "4 5 6"};
-     var data = {a:1, b:2, c:3};
-    //var json = JSON.stringify(wallet1.Addresses);
     
     $scope.downloadWallet = function(){
-      console.log('downloaded');
-      console.log(wallet1.Addresses);
-      chrome.downloads.download(obj);
-        return new Blob([wallet1.Addresses], {type: "application/json"});
+		console.log('downloaded');
+		console.log(wallet1.Addresses);
+		chrome.downloads.download(obj);
+		return new Blob([wallet1.Addresses], {type: "application/json"});
         
     }
-   $scope.generateLeAddress = function() {
-      $scope.currentAddress =  WalletManager.getCurrentWallet().generatePublicAddress();
-	    WalletManager.update(WalletManager.curWallet);
+	
+	$scope.generateAddress = function() {
+		$scope.currentAddress =  WalletManager.getCurrentWallet().generatePublicAddress();
+		WalletManager.update(WalletManager.curWallet);
     }
 
 	$scope.generateWallet = function(WalletName) {
 		if(!WalletName || WalletName.length == 0) {
-			console.log("meh");
+			throw new Error("Improper Wallet Name");
 		} else {
-			console.log(WalletName);
 			var wallet = new Wallet(WalletName);
 			WalletManager.addWallet(wallet);
 			$scope.wallets = WalletManager.getWallets();
 			$rootScope.curWallet = $scope.curWallet;
-			console.log(WalletManager.numWallets());
 		}
 	}
 
 	$scope.select = function(walletRef) {
+		if(!walletRef) {
+			throw new Error("Undefined Wallet");
+		}
 		WalletManager.setWalletR(walletRef);
 		$scope.curWallet = walletRef;
 		$rootScope.curWallet = $scope.curWallet;
-		console.log(walletRef);
 	};
-  $scope.remove = function(index) {
-      data.push($scope.wallets.splice(index, 1)[0]);
-  };
+	
+	$scope.remove = function(index) {
+		data.push($scope.wallets.splice(index, 1)[0]);
+	};
 
   //} )
 });//end Home Controller
