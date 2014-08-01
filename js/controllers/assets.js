f8e3c92056a092630d30b02a22779d687bf7fc14
leo <lshao30@gmail.com>
2014-07-31 20:59:06 -0400

    assets error
    added undef checks for add item

--- js/controllers/assets.js
+++ js/controllers/assets.js
@@ -1,27 +1,32 @@
 "use strict";
 cApp.controller("AssetsController",function($scope,Asset,DecentralStorage,WalletManager)  {
-    $scope.pageClass = 'page-assets';
+    
+	/************************Assets init*************************/
+	$scope.pageClass = 'page-assets';
     $scope.items = WalletManager.getCurrentWallet().getAllAssets();
-	
+	$scope.name=/^[a-zA-Z ]*$/;
+	$scope.integerval=/^\d*$/;
   	$scope.addAssetShow = false;
-    $scope.addItem = function(item) {
+	$scope.totalBTC = WalletManager.getCurrentWallet().getBalance();
+	/************************Assets init end***********************/
+    
+	$scope.addItem = function(item) {
+		if(!item || !item["Name"] || !item["BTC"] || !item["Units"]) {
+			throw new Error("Improper input");
+		}
+		
 		var hash = WalletManager.getCurrentWallet().generatePublicAddress();
 		var newAsset = new Asset(item["Name"],item["BTC"],item["Units"],hash);
 		$scope.itemForm.$setPristine();   
 		WalletManager.getCurrentWallet().addAsset(newAsset);
 		$scope.items = WalletManager.getCurrentWallet().getAllAssets();
 		WalletManager.updateCurrent();
-
 	};
-   
-    $scope.totalBTC = WalletManager.getCurrentWallet().getBalance();
+    
     $scope.mySortFunction = function(item) {
       if(isNaN(item[$scope.sortExpression]))
         return item[$scope.sortExpression];
       return parseInt(item[$scope.sortExpression]);
     };
     
-    $scope.name=/^[a-zA-Z ]*$/;
-    
-	$scope.integerval=/^\d*$/;
 });
